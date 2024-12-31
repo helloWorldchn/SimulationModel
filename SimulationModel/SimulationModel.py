1f0a6fc2a8926e6454203120b7e4e1b19de58cfb
ZhangMingYang <zhmychn@163.com>
2024-12-31 17:56:01 +0800

    feat: 时域仿真设计

--- SimulationModel/SimulationModel.py
+++ SimulationModel/SimulationModel.py
@@ -1,4 +1,6 @@
 from FreeFall import FreeFall
+from TimeDomainAnalysis import TimeDomainAnalysis
 
 if __name__ == '__main__':
     FreeFall(g=9.81,t_max = 10,dt = 0.1).freeFall()
+    TimeDomainAnalysis(1, 6, 25).timeDomainAnalysis()
